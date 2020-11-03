6de699598e662cc6eb5c3ae60d4ea8ff4156795c
Hahahahx <1219654535@qq.com>
2020-11-03 17:21:20 +0800

    'delete:console'

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -9,9 +9,24 @@ declare module "ux-redux-module" {
         children: any;
     }): JSX.Element;
     function useModule<T = {}>(): T;
+    /**
+     * 更新状态
+     * @function action**() 实现任意的action方法，添加Action装饰器，使用该注解必须确保return出一个promise对象
+     * @return new Promise.then(res=>{ this.name = res.name })
+     */
     function Action(target: any, property: string, descriptor: any): void;
+    /**
+     * 更新状态
+     * @function update() 实现update方法，添加Update装饰器在改变状态的action方法中调用该方法
+     */
     function Update(target: any, property: string, descriptor: any): void;
+    /**
+     * SessionStorage存储
+     */
     function SessionStorage(...props: any): void;
+    /**
+     * LocalStorage存储
+     */
     function LocalStorage(...props: any): void;
     export {
         ReduxProvider,
